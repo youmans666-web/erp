66a010f61a7fead052e05864832745cf4501f941
mldn <mldn@163.com>
2020-11-21 20:59:54 +0800

    change Emp.java

--- Emp.java
+++ Emp.java
@@ -1,4 +1,29 @@
-Hello hello = new Hello() ;
 public class Emp{
 	private Integer empno ;
+	private String ename ;
+	private Emp emps[] ;
+
+	public Integer getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(Integer empno) {
+		this.empno = empno;
+	}
+
+	public String getEname() {
+		return ename;
+	}
+
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+	public Emp[] getEmps() {
+		return emps;
+	}
+
+	public void setEmps(Emp[] emps) {
+		this.emps = emps;
+	}
 }
